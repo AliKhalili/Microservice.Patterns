03e6b16fe59c343ce79aca535eb87dc3de66a0fd
ali <akyegane@gmail.com>
2021-08-14 14:02:06 +0430

    Refactor: add comment for FlakyStrategy interface

--- FlakyApis/FlakyWeather/Implementations/IFlakyStrategy.cs
+++ FlakyApis/FlakyWeather/Implementations/IFlakyStrategy.cs
@@ -4,9 +4,26 @@ using System.Threading.Tasks;
 
 namespace FlakyApi.Implementations
 {
+    /// <summary>
+    /// Define an interface for changing the behavior of a function to flaky.
+    /// <para> flaky here means an unreliable function that when it called there is a chance raise <see cref="ServiceCurrentlyUnavailableException"/> exception </para>
+    /// </summary>
     public interface IFlakyStrategy
     {
+        /// <summary>
+        /// A wrapper for function which we want to turn its behavior for flaky.
+        /// </summary>
+        /// <typeparam name="TResult">the return type of given function</typeparam>
+        /// <param name="func"></param>
+        /// <param name="parameters">the input parameters of given function</param>
+        /// <returns>the return type of given function</returns>
+        /// <exception cref="ServiceCurrentlyUnavailableException"> Thrown when the flaky strategy want to change behavior to flaky</exception>
         Task<TResult> Execute<TResult>(Func<IDictionary<string, object>, Task<TResult>> func, IDictionary<string, object> parameters);
+        
+        /// <summary>
+        /// After a while we can rest the flaky behavior to initial version
+        /// </summary>
+        /// <returns>Task</returns>
         Task Reset();
     }
 }
